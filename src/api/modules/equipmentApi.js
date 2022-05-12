581b1019dcc0884b138d1a4d9ae3417f3b72556f
Ryangmoon <15398383460@163.com>
2022-05-12 10:56:37 +0800

    store in and out

--- src/api/modules/equipmentApi.js
+++ src/api/modules/equipmentApi.js
@@ -14,7 +14,7 @@ export const putPost = (params) => {
   return axios.put(`api/user`,params);
 }
 export const delDevice = (device_id,params) => {
-  return axios.delete(`/admin/deviceManage/deviceDel/${device_id}`,params);
+  return axios.delete(`/admin/deviceManage/deviceDel/device_id/${device_id}`,params);
 }
 
 export const getPostsOrderBy = (orderBy,params) => {
--- src/api/modules/storeApi.js
+++ src/api/modules/storeApi.js
@@ -14,6 +14,12 @@ export const delStore = (params) => {
   return axios.delete(`/admin/storeManage/storeDel/device_id`,params);
 }
 
+export const inStore = (device_id , num , params) => {
+  return axios.put(`admin/storeMange/storeIn/${device_id}/${num}`,params);
+}
+export const outStore = (device_id , params) => {
+  return axios.put(`admin/storeMange/storeOut/${device_id}`,params);
+}
 
 export const putPost = (params) => {
     return axios.put(`api/user`,params);
